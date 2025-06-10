a361f4b5642f9b0f83a34f8bddceb60d13eef841
Alice Okingo <annekadiso@gmail.com>
2025-06-10 17:22:06 +0300

    feat: implement the company sign up form

--- users/forms.py
+++ users/forms.py
@@ -92,3 +92,67 @@ class CustomerSignUpForm(UserCreationForm):
             birth=self.cleaned_data.get('birth')
         )
         return user       
+
+
+class CompanySignUpForm(UserCreationForm):
+    """
+    Form for company registration with:
+    - Company name (as username)
+    - Email (with validation)
+    - Field of work selection
+    - Password fields
+    """
+    
+    # Form field definitions
+    username = forms.CharField(
+        max_length=150,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your company name'
+        })
+    )
+    email = forms.EmailField(
+        max_length=100,
+        required=True,
+        validators=[validate_email],
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter company email address'
+        })
+    )
+    field = forms.ChoiceField(
+        choices=(
+            ('Air Conditioner', 'Air Conditioner'),
+            ('All in One', 'All in One'),
+            # ... other choices ...
+            ('Water Heaters', 'Water Heaters')
+        ),
+        required=True,
+        widget=forms.Select(attrs={
+            'class': 'form-control',
+            'placeholder': 'Select your field of work'
+        })
+    )
+    # Password fields same as CustomerSignUpForm...
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'field')
+
+    @transaction.atomic
+    def save(self):
+        """
+        Saves the user as a company with atomic transaction:
+        1. Creates User with is_company=True
+        2. Creates associated Company profile
+        """
+        user = super().save(commit=False)
+        user.is_company = True
+        user.email = self.cleaned_data.get('email')
+        user.save()
+        company = Company.objects.create(
+            user=user,
+            field=self.cleaned_data.get('field')
+        )
+        return user
