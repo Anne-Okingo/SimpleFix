6d32c6d9792e97f7957e38413d986610a554e15b
Anne-Okingo <annekadiso@gmail.com>
2025-08-16 13:10:39 +0300

    feat(users): implement CustomerSignUpForm, CompanySignUpForm and custom UserLoginForm with email uniqueness validation

--- users/forms.py
+++ users/forms.py
@@ -1,39 +1,75 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, authenticate
-from django.db import transaction
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.exceptions import ValidationError
 
 from .models import User, Company, Customer
 
 
+# ------------------------------------------------------------
+# Reusable date input widget
+# ------------------------------------------------------------
 class DateInput(forms.DateInput):
     input_type = 'date'
 
 
 def validate_email(value):
-    # In case the email already exists in an email input in a registration form, this function is fired
+    # Validators can be plugged into form fields
     if User.objects.filter(email=value).exists():
-        raise ValidationError(
-            value + " is already taken.")
+        raise ValidationError(f"{value} is already taken.")
 
 
+# ------------------------------------------------------------
+# 1) Customer Registration Form
+# ------------------------------------------------------------
 class CustomerSignUpForm(UserCreationForm):
-    pass
+    email = forms.EmailField(validators=[validate_email])
+    date_of_birth = forms.DateField(widget=DateInput())
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_customer = True
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+            Customer.objects.create(user=user, date_of_birth=self.cleaned_data["date_of_birth"])
+        return user
 
 
+# ------------------------------------------------------------
+# 2) Company Registration Form
+# ------------------------------------------------------------
 class CompanySignUpForm(UserCreationForm):
-    pass
+    email = forms.EmailField(validators=[validate_email])
+    field = forms.ChoiceField(choices=Company._meta.get_field("field").choices)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_company = True
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+            Company.objects.create(user=user, field=self.cleaned_data["field"])
+        return user
 
 
+# ------------------------------------------------------------
+# 3) Login form (email + password)
+# ------------------------------------------------------------
 class UserLoginForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(UserLoginForm, self).__init__(*args, **kwargs)
-
-    email = forms.EmailField(widget=forms.TextInput(
-        attrs={'placeholder': 'Enter Email'}))
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Enter Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'}))
 
     def __init__(self, *args, **kwargs):
-        super(UserLoginForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+        # turn off browser suggestions/autofill on email and password inputs
         self.fields['email'].widget.attrs['autocomplete'] = 'off'
+        self.fields['email'].widget.attrs['autocomplete'] = 'off'
+
